fe2121399a4f9f69a0997cc8070855fbc611caaf
Arshad <arshad@hobbiate.com>
2026-01-06 19:06:44 +0530

    now user can post

--- backend/apps/posts/views.py
+++ backend/apps/posts/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from django.shortcuts import redirect
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def home(request):
     posts = Post.objects.select_related("author").order_by("-created_at")
     return render(request, "home.html", {"posts": posts})
+
+
+@require_POST
+@login_required
+def create_post(request):
+    content = request.POST.get("content", "").strip()
+
+    if content:
+        Post.objects.create(
+            author=request.user,
+            content=content
+        )
+
+    return redirect("home")
