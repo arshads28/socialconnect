071df6a87c56c7e47c44072fa85e2508b5cd0e27
Arshad <arshad@hobbiate.com>
2026-01-15 00:29:18 +0530

    expo first commit

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import signup_view, login_view, logout_view, ProfileViewSet,password_reset_confirm_view,password_reset_request_view
 from django.contrib.auth import views as auth_views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 
@@ -20,9 +21,13 @@ urlpatterns = [
     path('reset-password/', password_reset_request_view, name='password_reset_request'),
     path('reset-password/confirm/', password_reset_confirm_view, name='password_reset_confirm'),
 
-    # path("profile/", profile_view, name="profile"),
-    # path("profile/edit/", edit_profile_view, name="edit_profile"),
-    # path('profile/<str:username>/', public_profile, name='public_profile'),
+    # MOBILE API VIEWS 
+    # The phone will POST to these URLs to get the Access Token
+    path("api/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+
+    
 ]
 
 urlpatterns += router.urls
